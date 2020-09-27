456ca5d75bc98e47fde1e268d79e7eabad6bd4e5
ccwyn <ccwyn93@163.com>
2020-09-27 14:57:12 +0800

    模板中使用外部数据

--- server.js
+++ server.js
@@ -2,25 +2,27 @@ const Vue = require("vue");
 const express = require("express");
 const renderer = require("vue-server-renderer").createRenderer();
 const server = express();
-const fs = require('fs')
-
+const fs = require("fs");
 
 server.get("/", (req, res) => {
   const app = new Vue({
-    template:fs.readFileSync('./index.templates.html','utf-8'),
-    // data: {
-    //   message: "你好",
-    // },
+    template: fs.readFileSync("./index.templates.html", "utf-8"),
   });
-  renderer.renderToString(app, (err, html) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).end("Internal Server Erroe");
-    }
+  renderer.renderToString(
+    app,
+    {
+      title: "拉钩教育",
+    },
+    (err, html) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).end("Internal Server Erroe");
+      }
 
-    res.setHeader('Content-Type', 'text/html; charset=utf8')
-    res.end(html)
-  });
+      res.setHeader("Content-Type", "text/html; charset=utf8");
+      res.end(html);
+    }
+  );
 });
 server.listen(3000, () => {
   console.log("server run at 3000!!!!!");
